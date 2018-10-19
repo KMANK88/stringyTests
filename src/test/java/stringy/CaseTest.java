290c9a9ce14e9af90092676f8511a826ee4d11b5
KMANK88 <karstenpease88@outlook.com>
2018-10-19 11:59:19 -0600

    added travis file

--- src/test/java/stringy/CaseTest.java
+++ src/test/java/stringy/CaseTest.java
@@ -6,21 +6,142 @@ import static org.junit.Assert.*;
 
 public class CaseTest {
 
-    @Test
-    public void alternatingDoesNotCrash() {
-        var output = Case.alternating("aaAA");
-        assertEquals("aAaA", output);
-    }
 
+    
+	//=====================1st test
     @Test
-    public void sentenceDoesNotCrash() {
+    public void lowerFirstCapLastAlternatingCase() {
+        var output = Case.alternating("helloTHERE");
+        assertEquals("hElLoThErE", output);
+        //normal works fine
+    }
+    
+    @Test
+    public void CapFirstLowerLastAlternatingCase() {
+        var output = Case.alternating("HELLOthere");
+        assertEquals("HeLlOtHeRe", output);
+        //reverse capital letters.
+    }
+    @Test
+    public void firstLowerCaseRestCapAlternatingCase() {
+        var output = Case.alternating("mYBABYBOY");
+        assertEquals("mYbAbYbOy", output);
+        //first not capital rest are
+    }
+    @Test
+    public void firstCapRestLowerCaseAlternatingCase() {
+        var output = Case.alternating("Yoarecool");
+        assertEquals("YoArEcOoL", output);
+        //first capital no others
+    }
+    
+    @Test
+    public void allLowerAlternatingCase() {
+        var output = Case.alternating("ab");
+        assertEquals("aB", output);
+        //no capital
+    }
+    @Test
+    public void AllCapitalAlternatingCase() {
+        var output = Case.alternating("AA");
+        assertEquals("Aa", output);
+        //all capital
+    }
+    /*
+    @Test
+    public void alternatingDoesNotCrash6() {
+        var output = Case.alternating("");
+        assertEquals("", output);
+        //fails on purpose for empty string.
+    }
+    */
+    
+  
+    @Test
+    public void withSlashAlternatingCase() {
+        var output = Case.alternating("A/a");
+        assertEquals("A/A", output);
+        //works, because it is start of new string, so next one will fail. 
+    }
+  
+    
+   
+    
+    
+//============================================
+    @Test
+    public void twoCapitalWordsSentenceTransform() {
         var output = Case.sentence("Hello World");
         assertEquals("Hello world", output);
-    }
-
+    }//upper case turn into lowercase
     @Test
-    public void titleDoesNotCrash() {
+    public void FirstCapsecondLowerCaseSentenceTransform() {
+        var output = Case.sentence("Hello there");
+        assertEquals("Hello there", output);
+    }
+    
+    public void AllCapitalWordsSentenceTransform() {
+        var output = Case.sentence("Hello There General");
+        assertEquals("Hello there general", output);
+    }
+    public void oneWordTwocapitalsSentenceTransform() {
+        var output = Case.sentence("HelloThere");
+        assertEquals("Hellothere", output);
+    }//no spaces
+
+    public void allCapSentenceTransformSentenceTransform() {
+        var output = Case.sentence("FTC");
+        assertEquals("Ftc", output);
+    }
+    public void AllCapWithSpacesSentenceTransform() {
+        var output = Case.sentence("F T C");
+        assertEquals("F t c", output);
+    }
+    public void sentenceWith2CapSentenceTransform() {
+        var output = Case.sentence("How are you Doing");
+        assertEquals("How are you doing", output);
+    }
+    public void twoCapWithPunctuationSentenceTransform() {
+        var output = Case.sentence("Hi. You ok?");
+        assertEquals("Hi. you ok?", output);
+    }
+    //=================================================
+    @Test
+    public void twoLowerCaseTitle() {
         var output = Case.title(("hello world"));
         assertEquals("Hello World", output);
     }
+    
+    @Test
+    public void firstLettersLowerCaseTitle() {
+        var output = Case.title(("hI tHERE"));
+        assertEquals("HI THERE", output);
+    }
+
+    @Test
+    public void sentenceWithTwoCapTitle() {
+        var output = Case.title(("I do say this is quite Fun."));
+        assertEquals("I Do Say This Is Quite Fun.", output);
+    }
+/*
+    @Test
+    public void titleDoesNotCrash3() {
+        var output = Case.title(("moore spaces  is   great"));
+        assertEquals("Moore Spaces  Is   Great", output);
+        //this fails when there is more than one space.
+    }
+    */
+    @Test
+    public void allLowerCaseTitle() {
+        var output = Case.title(("moore spaces is great"));
+        assertEquals("Moore Spaces Is Great", output);
+        //this fails when there is more than one space.
+    }
+    @Test
+    public void allLowerCaseLettersTitle() {
+        var output = Case.title(("aa bb cccDd"));
+        assertEquals("Aa Bb CccDd", output);
+        //this fails when there is more than one space.
+    }
+    
 }
--- src/test/java/stringy/FormatTest.java
+++ src/test/java/stringy/FormatTest.java
@@ -6,16 +6,107 @@ import static org.junit.Assert.*;
 
 public class FormatTest {
 
-    @Test
-    public void indentShouldNotCrash() {
+	@Test
+    public void twoIndentMultipleLines() {
         var output = Format.indent("hello\nworld", 2);
         assertEquals("  hello\n  world", output);
     }
-
+	
+	@Test
+    public void oneLine3Spaces() {
+        var output = Format.indent("hello there", 3);
+        assertEquals("   hello there", output);
+    }
+	@Test
+    public void noSpaces() {
+        var output = Format.indent("hello there", 0);
+        assertEquals("hello there", output);
+    }
+	@Test
+    public void twoIndentOnMultipleLines() {
+        var output = Format.indent("hello\n\nthere", 2);
+        assertEquals("  hello\n  \n  there", output);
+    }
+	@Test
+    public void multipleLinesNoIndent() {
+        var output = Format.indent("hello\n\nthere", 0);
+        assertEquals("hello\n\nthere", output);
+    }
+	@Test
+    public void spaceInfrontofIndent() {
+        var output = Format.indent(" hello\nthere", 1);
+        assertEquals("  hello\n there", output);
+    }
+	//============================================
+	//This function does not work--look into or it is a giant bug. 
     @Test
-    public void wordWrapShouldNotCrash() {
+    public void sinplewordwrap() {
         var output = Format.wordWrap("hello world", 5);
         assertEquals("hello\nworld", output);
 
-    }
+  } 
+    
+    
+    @Test
+    public void differentLevelsOfStringsWrap() {
+        var output = Format.wordWrap("aa aaa aaaaa aaaaaa", 6);
+        assertEquals("aa aaa\naaaaa\naaaaaa", output);
+
+  } 
+  
+    
+    @Test
+    public void stringlengthTwooffFromSpaceWrap() {
+        var output = Format.wordWrap("aaaa aaaaa aaaaaa", 6);
+        assertEquals("aaaa\naaaaa\naaaaaa", output);
+
+  } 
+    
+    @Test
+    public void stringlengthOneoffFromSpaceWrap() {
+        var output = Format.wordWrap("aaaaa aaaaa aaaaaa", 6);
+        assertEquals("aaaaa\naaaaa\naaaaaa", output);
+
+  } 
+    @Test
+    public void StringWrapGoesIntoSecondWordHalfWayThrough() {
+        var output = Format.wordWrap("aaa aaa aaaaa aaaaaa", 6);
+        assertEquals("aaa\naaa\naaaaa\naaaaaa", output);
+
+  } 
+    @Test
+    public void StringWrapWithSpaces() {
+        var output = Format.wordWrap("aa aaa  a aaaaaa", 9);
+        assertEquals("aa aaa  a\naaaaaa", output);
+
+  } 
+    @Test
+    public void StringWrapOnOneLine() {
+        var output = Format.wordWrap("aa aaa  a aaaaaa", 99);
+        assertEquals("aa aaa  a aaaaaa", output);
+
+  } 
+    
+    @Test
+    public void StringWrapLengthLineOne() {
+        var output = Format.wordWrap("a aa aaa aaaa", 1);
+        assertEquals("a\naa\naaa\naaaa", output);
+        //allwords will go to new line because they exceed 1 linelength. 
+
+  } 
+    
+    @Test
+    public void WrapStartingWithSpaces() {
+        var output = Format.wordWrap("      Yes    Nooo", 5);
+        assertEquals("Yes  \nNooo", output);
+
+  } 
+ 
+    
+    @Test
+    public void WrapEndinginSpace() {
+        var output = Format.wordWrap("Yoyo what k    ", 3);
+        assertEquals("Yoyo\nwhat\nk", output);
+
+  } 
 }
